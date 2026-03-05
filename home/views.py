0de47c092ba4c829332bd7667744b5ef28c86606
KcVic <akomavictor@gmail.com>
2026-03-05 17:02:36 +0100

    Created home app url.py. Linked hpme app to project level url. Created views for home page and about page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .models import about, Skill
 
 # Create your views here.
-def index(request):
-    return HttpResponse('this is the home page')
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
+
+class AboutView(TemplateView):
+    template_name = 'home/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['about'] = about.objects.all()
+        context['skill'] = Skill.objects.all()
+        return context
