a5283fec527e6c7a15d63361c265e48e4411bfa2
KcVic <akomavictor@gmail.com>
2026-03-05 17:11:47 +0100

    created projects view

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('projects/', include('projects.urls')),
     path('admin/', admin.site.urls),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import project
 
 # Create your views here.
+class ProjectsView(TemplateView):
+    template_name = 'projects/projects.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['projects'] = project.objects.all()
+        return context
