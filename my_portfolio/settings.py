03929bd43dbbd88a6d7d985452d4b116cc7bd322
KcVic <akomavictor@gmail.com>
2026-03-07 16:10:11 +0100

    Setup email forwarding link

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -135,8 +135,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = [EMAIL_ADDRESS]'
-EMAIL_HOST_PASSWORD = '[PASSWORD]'
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_ADDRESS')
 
 # Media files (Images, PDFs) uploads
 MEDIA_URL = '/media/'
