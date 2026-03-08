3f6a8f1abbbefac4ec09db0989a62c8564082e3f
KcVic <akomavictor@gmail.com>
2026-03-08 09:50:19 +0100

    uploade headshot to static files and ran collectstatic

--- home/models.py
+++ home/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
 class about(models.Model):
     title = models.CharField(max_length=100, null=True, blank=True)
     body = models.TextField(null=True, blank=True)
-    profile_pic = models.ImageField(upload_to='images/', null=True, blank=True)
+    profile_pic = CloudinaryField('image', null=True, blank=True)
     github = models.URLField(null=True, blank=True)
     linkedin = models.URLField(null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
